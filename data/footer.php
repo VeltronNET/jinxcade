ad2dfe7144f142cfa446dba622c84bb61d00a076
JinxCade <76182826+JinxCade@users.noreply.github.com>
2026-04-17 19:02:54 -0500

    v1.4.1 (v2 beta)

--- data/footer.php
+++ data/footer.php
@@ -2,7 +2,7 @@
 <div class="status-bar">
     <p class="status-bar-field">jinxcade.veltron.net - 2023-2026</p>
     <p class="status-bar-field">Veltron - 2019-2026</p>
-    <p class="status-bar-field">Website version: v2 BETA (v1.4.0) - Last updated: 2026-04-13, 06:07 UTC</p>
+    <p class="status-bar-field">Website version: v2 BETA (v1.4.1) - Last updated: 2026-04-18, 00:02 UTC</p>
   </div>
 </div>
 </div>
